14249d60cfd29ea79f0e8bddd96e95d2a4b29301
Ezequiel Aceto <ezequiel.aceto@gmail.com>
2023-09-23 19:14:14 +0200

    Test

--- CICDApp/ContentView.swift
+++ CICDApp/ContentView.swift
@@ -8,14 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var value = false
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Toggle("Input Value", isOn: $value)
+            HStack {
+                Text("Output Value")
+                Spacer()
+                Text(Bit.from(value).invert().description)
+            }
         }
         .padding()
+    }
+}
+
+enum Bit: CustomStringConvertible {
+    case zero
+    case one
+                
+    static func from(_ boolean: Bool) -> Bit {
+        return boolean ? .one : .zero
+    }
+    
+    func invert() -> Bit {
+        return self == .zero ? .one : .zero
+    }
+    
+    var description: String {
+        self == .zero ? "0" : "1"
     }
 }
 
