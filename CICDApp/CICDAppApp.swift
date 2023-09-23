f1541074892596a66b17d763f14ba3e4439d5b3d
Ezequiel Aceto <ezequiel.aceto@gmail.com>
2023-09-23 18:16:36 +0200

    Initial Commit

---
+++ CICDApp/CICDAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICDAppApp.swift
+//  CICDApp
+//
+//  Created by Kimi on 23/9/23.
+//
+
+import SwiftUI
+
+@main
+struct CICDAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
